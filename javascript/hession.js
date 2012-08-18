40e0297e5268ac17fea924e7ab454226db1d6d4d
Peter Armstrong <peter@peter>
2012-08-18 10:21:45 +0100

    cleaned

---
+++ javascript/hession.js
@@ -0,0 +1,96 @@
+
+$(document).ready(function(){
+
+<!--      $("#paint").show();-->
+<!--      $("#repair").show();-->
+<!--      $("#build").show();-->
+<!--      $("#weld").show();-->
+<!--      -->
+      $("#paint_details").hide();
+
+
+<!--paint -->
+
+    $('#paint' || '#paint_menu').click(function()
+    {
+      $("#paint").toggle();
+      $("#paint_details").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+    $('#hide_paint').click(function()
+    {
+      $("#paint_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+<!--Repair-->
+
+    $('#repair').click(function()
+    {
+      $("#repair_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+    $('#hide_repair').click(function()
+    {
+      $("#repair_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+<!--  Build  -->
+    $('#build').click(function()
+    {
+      $("#build_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+    $('#hide_build').click(function()
+    {
+      $("#build_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+<!--Weld-->
+    $('#weld').click(function()
+    {
+      $("#weld_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+
+    $('#hide_weld').click(function()
+    {
+      $("#weld_details").toggle();
+
+      $("#paint").toggle();
+      $("#build").toggle();
+      $("#repair").toggle();
+      $("#weld").toggle();
+    });
+});
